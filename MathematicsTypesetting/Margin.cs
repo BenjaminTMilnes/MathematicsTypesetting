7de815aff347a199720a67506b9bc95563d4fe71
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2017-10-08 14:19:32 +0100

    Added Margin, as well as position, width, and height attributes to Number

---
+++ MathematicsTypesetting/Margin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicsTypesetting
+{
+    public class Margin
+    {
+        public Length Top { get; set; }
+        public Length Right { get; set; }
+        public Length Bottom { get; set; }
+        public Length Left { get; set; }
+    }
+}
--- MathematicsTypesetting/Number.cs
+++ MathematicsTypesetting/Number.cs
@@ -10,7 +10,17 @@ namespace MathematicsTypesetting
     {
         public string Content { get; set; }
 
+        public Length X { get; set; }
+        public Length Y { get; set; }
+
+        public Length Width { get; set; }
+        public Length Height { get; set; }
+
+        public Margin OuterMargin { get; set; }
+        public Margin InnerMargin { get; set; }
+
         public Colour BackgroundColour { get; set; }
+
         public FontStyle FontStyle { get; set; }
     }
 }
