1ef3c60e2405ffb21a2940b717ba2fb3a203ff9a
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2017-10-08 14:24:44 +0100

    Added Size and ITextMeasurer

---
+++ MathematicsTypesetting/ITextMeasurer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicsTypesetting
+{
+    public interface ITextMeasurer
+    {
+        Size MeasureTextSize(string text, FontStyle fontStyle);
+    }
+}
---
+++ MathematicsTypesetting/Size.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicsTypesetting
+{
+    public class Size
+    {
+        public Length Width { get; set; }
+        public Length Height { get; set; }
+    }
+}
