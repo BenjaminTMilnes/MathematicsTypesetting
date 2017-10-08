8c65dc20cf4449a43b95271b50233f10e0326ebf
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2017-10-08 13:10:51 +0100

    Created a basic class and established styling

---
+++ MathematicsTypesetting/FontStyle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicsTypesetting
+{
+    public class FontStyle
+    {
+        public string FontName { get; set; }
+    }
+}
