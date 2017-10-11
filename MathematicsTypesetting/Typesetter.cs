1269127d535fa4b56b5b28e9b0a36e4851afa089
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-10-11 21:12:37 +0100

    Added a function to Typesetter to find the size of a MathematicsLine

--- MathematicsTypesetting/Typesetter.cs
+++ MathematicsTypesetting/Typesetter.cs
@@ -15,8 +15,54 @@ namespace MathematicsTypesetting
             _textMeasurer = textMeasurer;
         }
 
-         public void SetMathematicsLineSize(MathematicsLine mathematicsLine)
+        public void SetMathematicsLineSize(MathematicsLine mathematicsLine)
         {
+            if (mathematicsLine.Elements.Any())
+            {
+                var elements = mathematicsLine.Elements.ToArray();
+
+                Element elementM = null;
+                Element elementN = null;
+                Length maximumContentHeight = 0;
+
+                for (var n = 0; n < elements.Length; n++)
+                {
+                    elementN = elements[n];
+
+                    // Add the width of the current element.
+                    mathematicsLine.SizeOfContent.Width += elementN.SizeIncludingBorder.Width;
+
+                    if (n > 0)
+                    {
+                        elementM = elements[n - 1];
+
+                        // Add the spacing of the margins of the current element and the previous element.
+                        mathematicsLine.SizeOfContent.Width += ChooseGreaterLength(elementM.OuterMargin.Right, elementN.OuterMargin.Left);
+                    }
+
+                    // If this element is taller, update the maximum content height.
+                    if (elementN.SizeIncludingBorder.Height > maximumContentHeight)
+                    {
+                        maximumContentHeight = elementN.SizeIncludingBorder.Height;
+                    }
+                }
+
+                mathematicsLine.SizeOfContent.Height = maximumContentHeight;
+            }
+            else
+            {
+                mathematicsLine.SizeOfContent = 0;
+            }
+        }
+
+        protected Length ChooseGreaterLength(Length length1, Length length2)
+        {
+            if (length1 > length2)
+            {
+                return length1;
+            }
+
+            return length2;
         }
 
         public void SetNumberSize(Number number)
