bdb81b27f622ba68d6f2c3e86463c7f314a89fe1
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-10-11 19:53:27 +0100

    Added MathematicsLine

---
+++ MathematicsTypesetting/MathematicsLine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicsTypesetting
+{
+    /// <summary>
+    /// Represents a line of mathematics where the elements in the line are aligned vertically.
+    /// </summary>
+    public class MathematicsLine
+    {
+        public IList<Element> Elements { get; set; }
+
+        public MathematicsLine()
+        {
+            Elements = new List<Element>();
+        }
+    }
+}
