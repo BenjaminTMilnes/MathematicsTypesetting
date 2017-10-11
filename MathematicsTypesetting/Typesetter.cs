0dbb5cf236eb7c5dbd8734a94f2ca3984b86c86b
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-10-11 20:19:01 +0100

    Made functions in Typesetter to set the size of basic elements like Number

--- MathematicsTypesetting/Typesetter.cs
+++ MathematicsTypesetting/Typesetter.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace MathematicsTypesetting
 {
-   public  class Typesetter
+    public class Typesetter
     {
+        protected ITextMeasurer _textMeasurer;
+
+        public Typesetter(ITextMeasurer textMeasurer)
+        {
+            _textMeasurer = textMeasurer;
+        }
+
+         public void SetMathematicsLineSize(MathematicsLine mathematicsLine)
+        {
+        }
+
+        public void SetNumberSize(Number number)
+        {
+            number.SizeOfContent = _textMeasurer.MeasureTextSize(number.Content, number.FontStyle);
+            number.SizeIncludingInnerMargin = AddMarginToSize(number.SizeOfContent, number.InnerMargin);
+
+            number.SizeIncludingOuterMargin = AddMarginToSize(number.SizeIncludingInnerMargin, number.OuterMargin);
+        }
+
+        protected Size AddMarginToSize(Size size, Margin margin)
+        {
+            var newSize = new Size();
+
+            newSize.Width = size.Width + margin.Left + margin.Right;
+            newSize.Height = size.Height + margin.Top + margin.Bottom;
+
+            return newSize;
+        }
+
+        protected Size AddBorderToSize(Size size)
+        {
+            var newSize = new Size();
+
+            return newSize;
+        }
     }
 }
