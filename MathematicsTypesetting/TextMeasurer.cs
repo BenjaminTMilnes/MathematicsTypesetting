3aaad01b23e97e76a0d13eb7f5c7c6cf840ee138
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-10-11 20:18:02 +0100

    Created an empty TextMeasurer for now

---
+++ MathematicsTypesetting/TextMeasurer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicsTypesetting
+{
+    public class TextMeasurer : ITextMeasurer
+    {
+        public Size MeasureTextSize(string text, FontStyle fontStyle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
