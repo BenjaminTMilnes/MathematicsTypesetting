aa173002f9caa1b1ff9c1eedfc95f53b9ef00a12
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-10-11 19:53:42 +0100

    Added Position

---
+++ MathematicsTypesetting/Position.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicsTypesetting
+{
+    /// <summary>
+    /// Represents the position of an element relative to the origin; a very simplified form of vector.
+    /// </summary>
+    public class Position
+    {
+        public Length X { get; set; }
+        public Length Y { get; set; }
+
+        public Position()
+        {
+            X = new Length(0, LengthUnits.Millimetres);
+            Y = new Length(0, LengthUnits.Millimetres);
+        }
+    }
+}
