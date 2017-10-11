d779929e7de7c057fd559152ffec0261373b5311
Benjamin T Milnes <benjamintmilnes@googlemail.com>
2017-10-11 20:48:55 +0100

    Allowed for setting a size to zero

--- MathematicsTypesetting/Size.cs
+++ MathematicsTypesetting/Size.cs
@@ -6,9 +6,40 @@ using System.Threading.Tasks;
 
 namespace MathematicsTypesetting
 {
+    public class UnableToConvertToSizeException : Exception
+    {
+        public object Object { get; private set; }
+
+        public UnableToConvertToSizeException(object object1)
+        {
+            Object = object1;
+        }
+
+        public override string ToString()
+        {
+            return $"Cannot implicitly convert {Object.ToString()} to a size.";
+        }
+    }
+
     public class Size
     {
         public Length Width { get; set; }
         public Length Height { get; set; }
+
+        public Size()
+        {
+            Width = 0;
+            Height = 0;
+        }
+
+        public static implicit operator Size(int i)
+        {
+            if (i == 0)
+            {
+                return new Size();
+            }
+
+            throw new UnableToConvertToSizeException(i);
+        }
     }
 }
