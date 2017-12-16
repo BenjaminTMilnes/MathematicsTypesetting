9b30efe07c1f92d689e2c048886c3931ea2c4978
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2017-12-16 19:54:58 +0000

    Added Fraction

---
+++ MathematicsTypesetting/Fraction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicsTypesetting
+{
+    public class Fraction : Element
+    {
+        public Element Numerator { get; set; }
+        public Element Denominator { get; set; }
+
+        public Colour BackgroundColour { get; set; }
+
+        public Fraction()
+        {
+            BackgroundColour = new Colour();
+        }
+    }
+}
