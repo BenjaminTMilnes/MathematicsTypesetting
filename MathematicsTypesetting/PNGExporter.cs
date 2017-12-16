2da746f576f3fc6e764e14e7559a64f8c928c5e5
Benjamin T. Milnes <benjamintmilnes@googlemail.com>
2017-12-16 15:45:57 +0000

    Added Document and PNGExporter

---
+++ MathematicsTypesetting/PNGExporter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace MathematicsTypesetting
+{
+    public class PNGExporter : Exporter
+    {
+        public void ExportMathematics(Document document, string fileLocation)
+        {
+            using (var bitmap = new Bitmap(100, 100))
+            {
+                using (var graphics = Graphics.FromImage(bitmap))
+                {
+
+                }
+
+                bitmap.Save(fileLocation, ImageFormat.Png);
+            }
+        }
+    }
+}
